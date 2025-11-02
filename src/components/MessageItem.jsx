dde685382ba71d27a669481e865475f054f7bbaa
21chrisab <DJPugst3r@gmail.com>
2025-11-02 13:16:32 -0600

    v1.0.20: Fix task checkbox to directly toggle completion instead of showing bulk actions

--- src/components/MessageItem.jsx
+++ src/components/MessageItem.jsx
@@ -174,7 +174,7 @@ function MessageItem({ message, onEdit, onDelete }) {
                             {message.type === 'file' && message.file_url && (
                                 <a href={message.file_url} target="_blank" rel="noopener noreferrer" 
                                    className={`flex items-center gap-2 mt-2 p-2 rounded-md ${isCurrentUser ? 'bg-blue-700 hover:bg-blue-800' : 'bg-gray-200 hover:bg-gray-300'}`}>
-                                    <Icon name="paperclip" className="w-5 h-5 flex-shrink-0" />
+                                    <Icon path="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13" className="w-5 h-5 flex-shrink-0" />
                                     <span className="text-sm font-medium truncate">{message.file_name}</span>
                                 </a>
                             )}
@@ -188,7 +188,7 @@ function MessageItem({ message, onEdit, onDelete }) {
                                             className="p-1 hover:bg-white hover:bg-opacity-20 rounded"
                                             title="Edit message"
                                         >
-                                            <Icon name="edit" className="w-4 h-4" />
+                                            <Icon path="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" className="w-4 h-4" />
                                         </button>
                                         <button
                                             onClick={handleDeleteMessage}
@@ -196,7 +196,7 @@ function MessageItem({ message, onEdit, onDelete }) {
                                             className="p-1 hover:bg-white hover:bg-opacity-20 rounded text-red-300 hover:text-red-200"
                                             title="Delete message"
                                         >
-                                            <Icon name="trash" className="w-4 h-4" />
+                                            <Icon path="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" className="w-4 h-4" />
                                         </button>
                                     </div>
                                 </div>
@@ -211,7 +211,7 @@ function MessageItem({ message, onEdit, onDelete }) {
                         onClick={() => setShowReactions(!showReactions)}
                         className="text-gray-400 hover:text-gray-600 text-sm"
                     >
-                        <Icon name="smile" className="w-4 h-4" />
+                        <Icon path="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" className="w-4 h-4" />
                     </button>
                     
                     {showReactions && (
