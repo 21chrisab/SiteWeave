67e854b54aa4d41de48a8b0de353916584b7beb8
21chrisab <DJPugst3r@gmail.com>
2026-01-07 23:44:59 -0600

    fix: Add ToastProvider to wrap app and fix useToast context error

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App.jsx'
 import ErrorBoundary from './components/ErrorBoundary.jsx'
 import { AppProvider } from './context/AppContext'
+import { ToastProvider } from './context/ToastContext'
 import './index.css'
 
 // Unregister any existing service workers to prevent caching issues
@@ -22,9 +23,11 @@ const router = createBrowserRouter([
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ErrorBoundary>
-      <AppProvider>
-        <RouterProvider router={router} />
-      </AppProvider>
+      <ToastProvider>
+        <AppProvider>
+          <RouterProvider router={router} />
+        </AppProvider>
+      </ToastProvider>
     </ErrorBoundary>
   </React.StrictMode>
 )
